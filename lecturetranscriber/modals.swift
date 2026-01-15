0bc4947e233e5c8e577f6ff9ba6e560ffd58b337
jt11-11 <jaspertsh08@gmail.com>
2026-01-15 17:43:10 +0800

    swapped out current model for swiftdata

--- lecturetranscriber/modals.swift
+++ lecturetranscriber/modals.swift
@@ -6,22 +6,42 @@
 //
 import SwiftUI
 import Foundation
+import SwiftData
 
-struct Lecture: Identifiable {
-    let id = UUID()
-    let title: String
-    let date: String
-    let duration: String
-    let summary: String
+
+@Model
+final class Lecture{
+    var id: UUID = UUID()
+    var title: String
+    var date: String
+    var duration: String
+    var summary: String
+    var course: Course?
+    
+    init(title: String, date: String, duration: String, summary: String) {
+        self.title = title
+        self.date = date
+        self.duration = duration
+        self.summary = summary
+    }
 }
 
-struct Course: Identifiable {
-    let id = UUID()
-    let name: String
-    let code: String
-    let schedule: String
-    let themeColor: Color
-    let icon: String
-    let lectures: [Lecture]
-}
+@Model
+final class Course {
+    var name: String
+    var code: String
+    var schedule: String
+    var themeColorHex: String
+    var icon: String
+    
+    @Relationship(deleteRule: .cascade)
+    var lectures: [Lecture] = []
 
+    init(name: String, code: String, schedule: String, themeColorHex: String, icon: String) {
+        self.name = name
+        self.code = code
+        self.schedule = schedule
+        self.themeColorHex = themeColorHex
+        self.icon = icon
+    }
+}
